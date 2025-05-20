69444d92d925d4b89fb727c2c0bbfaba4b3e9d47
Lev Pakhomov <l.pakhomov@vkteam.ru>
2025-05-21 01:17:08 +0300

    add visualization

--- visualize_with_networkx.py
+++ visualize_with_networkx.py
@@ -2,20 +2,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from mygraphlib import Graph
 
-# 1. Build a simple graph using your library
+# Build a simple graph using mygraphlib
 g = Graph()
 g.add_edge("0", "1")
 g.add_edge("1", "2")
 g.add_edge("1", "3")
 g.add_edge("3", "4")
 
-# 2. Convert your internal graph to networkx Graph
+# Convert internal graph to networkx Graph
 G = nx.Graph()
 for node, neighbors in g.adjacency.items():
     for neighbor in neighbors:
         G.add_edge(node, neighbor)
 
-# 3. Draw the graph using circular layout
+# Draw the graph using circular layout
 plt.figure(figsize=(6, 6))
 nx.draw_circular(
     G,
